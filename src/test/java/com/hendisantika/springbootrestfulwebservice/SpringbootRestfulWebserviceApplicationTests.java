1baa5066ceb5c764fd250fdaa28ffb9de4c10515
hendisantika <hendisantika@yahoo.co.id>
2019-07-14 08:29:05 +0700

    #1 - Initial commit

---
+++ src/test/java/com/hendisantika/springbootrestfulwebservice/SpringbootRestfulWebserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springbootrestfulwebservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootRestfulWebserviceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
