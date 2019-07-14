e1f3380706671f7338f9fbdd2a397dbfa92abcc7
hendisantika <hendisantika@yahoo.co.id>
2019-07-14 08:30:28 +0700

    #3 - Add Employee Model

---
+++ src/main/java/com/hendisantika/springbootrestfulwebservice/model/Employee.java
@@ -0,0 +1,19 @@
+package com.hendisantika.springbootrestfulwebservice.model;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-restful-webservice
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-07-14
+ * Time: 08:30
+ */
+@Data
+public class Employee {
+    private int id;
+    private String name;
+    private double salary;
+}
