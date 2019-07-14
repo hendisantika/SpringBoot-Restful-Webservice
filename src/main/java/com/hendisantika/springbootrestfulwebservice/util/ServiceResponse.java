975c74a43c4a7496536b0d57641acdcd745442c7
hendisantika <hendisantika@yahoo.co.id>
2019-07-14 08:31:58 +0700

    #5 - Add ServiceResponse

---
+++ src/main/java/com/hendisantika/springbootrestfulwebservice/util/ServiceResponse.java
@@ -0,0 +1,19 @@
+package com.hendisantika.springbootrestfulwebservice.util;
+
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-restful-webservice
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-07-14
+ * Time: 08:31
+ */
+public class ServiceResponse {
+    private Boolean success;
+    private String message;
+    private Map<String, Object> params;
+
+}
